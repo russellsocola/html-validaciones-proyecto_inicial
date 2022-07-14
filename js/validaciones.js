7d1c8928ecb87d37001cd15db5e0ff8bafac682a
russellsocola <russell999nahum@gmail.com>
2022-07-14 10:08:16 -0500

    agregamos data-tipo para refactorizar los mensajes de error.

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,8 +3,39 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
     };
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.classList.remove("input-message-error").innerrHTML= "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerrHTML= mostrartMensajesDeError(tipoDeInput,input);
+    }
 };
 
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMissing",
+    "patterMismatch",
+    "customError",
+];
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMissing: "Este correo no es valido",
+    },
+    password: {
+        valueMissing: "Es campo contraseña no puede estar vacio",
+        patterMismatch: "Al menos 6 caracteres, maximo 12, debe contener un Mayuscula, un letra minuscula, y un numero",
+    },
+    nacimiento: {
+        valueMissing: "El campo fecha no puede estar vacio",
+        customError: "debes tener al menos 18 años de edad pilluelo",
+    },
+}
 const validadores = {
     nacimiento: (input)=> validarNacimiento(input),
 };
@@ -13,10 +44,22 @@ function validarNacimiento(input){
     const fechaCliente = new Date(input.value);
     let mensaje = "";
     if(!mayorEdad(fechaCliente)){
-        mensaje = "Debes tener al menos 18 años pilluelo"
-    }
-    
+        mensaje = "Debes tener al menos 18 años de edad pilluelo";
+    };
     input.setCustomValidity(mensaje);
+}
+
+function mostrartMensajesDeError(tipoDeInput,input){
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if(input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje= mensajesDeError[tipoDeInput][error];
+        };
+    });
+return mensaje;
 }
 
 function mayorEdad (fecha){
